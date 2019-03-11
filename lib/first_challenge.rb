dbc8aac26676d8ffcaacaaf4c124be059781497a
Danielle Jasper <dnj547@live.com>
2019-03-11 16:02:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,8 +18,8 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         :favorite_icecream_flavors.delete("strawberry")
-      end    
-    end  
+      end
+    end
   end
   #remember to return your newly altered contacts hash!
   contacts
